4472d21de8f2faaead36391e05ccdb145e4ffc6a
Andrej Becka <beckaand@pop-os.localdomain>
2023-05-25 14:01:42 +0200

    SEM - Implement the general structure of PlayState

---
+++ SemestralWork/src/PlayState.cpp
@@ -0,0 +1,26 @@
+#include "PlayState.hpp"
+#include <iostream>
+
+const std::string PlayState::s_playID = "PLAY";
+
+void PlayState::update()
+{
+// nothing for now
+}
+
+void PlayState::render()
+{
+// nothing for now
+}
+
+bool PlayState::onEnter()
+{
+    std::cout << "entering PlayState\n";
+    return true;
+}
+
+bool PlayState::onExit()
+{
+    std::cout << "exiting PlayState\n";
+    return true;
+}
---
+++ SemestralWork/src/PlayState.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "GameState.hpp"
+
+class PlayState : public GameState
+{
+public:
+    virtual void update();
+    virtual void render();
+    virtual bool onEnter();
+    virtual bool onExit();
+    virtual std::string getStateID() const { return s_playID; }
+
+private:
+    static const std::string s_playID;
+};
