6f8cf2423ba5fab17032bb2130970cc5dce22cc0
Andrej Becka <beckaand@pop-os.localdomain>
2023-05-25 12:22:53 +0200

    SEM - Implement SDLGameObject class, a subclass of GameObject

---
+++ SemestralWork/src/SDLGameObject.cpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "TextureManager.hpp"
+#include "GameObject.hpp"
+#include "Vector2D.hpp"
+
+class SDLGameObject : public GameObject
+{
+public:
+    SDLGameObject(const LoaderParams* pParams);
+    virtual void draw();
+    virtual void update();
+    virtual void clean();
+
+protected:
+    Vector2D m_position;
+    Vector2D m_velocity;
+    Vector2D m_acceleration;
+
+    int m_width;
+    int m_height;
+    int m_currentRow;
+    int m_currentFrame;
+    std::string m_textureID;
+};
