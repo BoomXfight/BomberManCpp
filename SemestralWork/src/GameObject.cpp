c7719b308d2312917b7d3d3d655f8168951a14df
Andrej Becka <beckaand@pop-os.localdomain>
2023-05-25 12:11:10 +0200

    SEM - Implement GameObject base class

---
+++ SemestralWork/src/GameObject.cpp
@@ -0,0 +1,5 @@
+#include "GameObject.hpp"
+
+GameObject::GameObject(const LoaderParams* pParams) {}
+
+GameObject::~GameObject() {}
---
+++ SemestralWork/src/GameObject.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "LoaderParams.hpp"
+
+class GameObject
+{
+public:
+    virtual void draw() = 0;
+    virtual void update() = 0;
+    virtual void clean() = 0;
+
+protected:
+    GameObject(const LoaderParams* pParams);
+    virtual ~GameObject();
+};
