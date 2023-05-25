eab0d931845c9cab0dd53ffd346783cc76dbb8e9
Andrej Becka <beckaand@pop-os.localdomain>
2023-05-25 13:56:59 +0200

    SEM - Implement Enemy class

---
+++ SemestralWork/src/Enemy.cpp
@@ -0,0 +1,41 @@
+#include "Enemy.hpp"
+#include "InputHandler.hpp"
+
+Enemy::Enemy(const LoaderParams* pParams) :
+        SDLGameObject(pParams)
+{}
+
+void Enemy::draw()
+{
+    SDLGameObject::draw();
+}
+
+void Enemy::update()
+{
+    handleInput();
+    m_currentFrame = int(((SDL_GetTicks() / 100) % 8));
+    SDLGameObject::update();
+}
+
+void Enemy::clean()
+{}
+
+void Enemy::handleInput()
+{
+    if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_D))
+    {
+        m_position += Vector2D(2,0);
+    }
+    if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_A))
+    {
+        m_position += Vector2D(-2,0);
+    }
+    if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_W))
+    {
+        m_position += Vector2D(0,-2);
+    }
+    if(TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_S))
+    {
+        m_position += Vector2D(0,2);
+    }
+}
---
+++ SemestralWork/src/Enemy.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "SDLGameObject.hpp"
+
+class Enemy : public SDLGameObject
+{
+public:
+    Enemy(const LoaderParams* pParams);
+    virtual void draw();
+    virtual void update();
+    virtual void clean();
+
+private:
+    void handleInput();
+};
