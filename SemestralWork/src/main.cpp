69405cb42aaed2af2d88df8e48dbd11502865075
Andrej Becka <beckaand@pop-os.localdomain>
2023-05-25 11:55:38 +0200

    SEM - Implement main

---
+++ SemestralWork/src/main.cpp
@@ -0,0 +1,42 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include "Game.hpp"
+
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
+int main(int argc, char* argv[])
+{
+    Uint32 frameStart, frameTime;
+
+    std::cout << "game init attempt...\n";
+    if(TheGame::Instance()->init("BomberMan", 0, 0, 960, 540, false))
+    {
+        std::cout << "game init success!\n";
+        while(TheGame::Instance()->running()) // Game loop
+        {
+            frameStart = SDL_GetTicks();
+
+            TheGame::Instance()->handleEvents();
+            TheGame::Instance()->update();
+            TheGame::Instance()->render();
+
+            frameTime = SDL_GetTicks() - frameStart;
+            if(frameTime < DELAY_TIME) // Takes care of a 60 FPS
+            {
+                SDL_Delay((int)(DELAY_TIME - frameTime));
+            }
+        }
+    }
+    else
+    {
+        std::cout << "game init failure - " << SDL_GetError() << "\n";
+        return -1;
+    }
+
+    std::cout << "game closing...\n";
+    TheGame::Instance()->clean();
+    return 0;
+}
+
+
