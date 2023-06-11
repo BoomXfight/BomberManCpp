4bf544a734fb2aa533bf2e036b897e3b0d0a5d0e
Andrej Becka <beckaand@pop-os.localdomain>
2023-06-12 01:06:32 +0200

    SEM - Write MenuState documentation

--- SemestralWork/src/GameStates/MenuState.hpp
+++ SemestralWork/src/GameStates/MenuState.hpp
@@ -2,12 +2,23 @@
 #include "../GameObjects/GameObject.hpp"
 #include "GameState.hpp"
 
-
+/**
+ * @class MenuState
+ * This is a base class for all the menu states throughout the game.
+ */
 class MenuState : public GameState
 {
 public:
     MenuState();
+
+    /**
+     * This method updates the MenuState.
+     */
     void update() override;
+
+    /**
+     * This method renders the MenuState to the screen.
+     */
     void render() override;
 
 protected:
