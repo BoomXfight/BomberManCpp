5dd5059d0875a6c9b965aba2165d9b7e149d8689
Andrej Becka <beckaand@pop-os.localdomain>
2023-06-12 00:56:42 +0200

    SEM - Write GameState class documentation

--- SemestralWork/src/GameStates/GameState.hpp
+++ SemestralWork/src/GameStates/GameState.hpp
@@ -4,17 +4,36 @@
 
 /**
  * @class GameState
- *
+ * This is a base class for every GameState in the game
  */
 class GameState
 {
 public:
     GameState();
     virtual ~GameState();
+
+    /**
+     * This class will update the GameState
+     */
     virtual void update() = 0;
+
+    /**
+     * This class will render the GameState
+     */
     virtual void render() = 0;
+
+    /**
+     * This class will load up the GameState upon entering it
+     * @return
+     */
     virtual bool onEnter() = 0;
+
+    /**
+     * This class will clean after the GameState at the end of it
+     * @return
+     */
     virtual bool onExit() = 0;
+
     virtual std::string getStateID() const = 0;
 
 protected:
