8a167480fa545cd303299f546e1003ffd367ee39
Andrej Becka <beckaand@pop-os.localdomain>
2023-06-12 01:10:07 +0200

    SEM - Write MultiPlayer1WinState documentation

--- SemestralWork/src/GameStates/MultiPlayer1WinState.hpp
+++ SemestralWork/src/GameStates/MultiPlayer1WinState.hpp
@@ -1,11 +1,24 @@
 #pragma once
 #include "MenuState.hpp"
 
+/**
+ * @class MultiPlayer1WinState
+ */
 class MultiPlayer1WinState : public MenuState
 {
 public:
+    /**
+     * This method initializes the MultiPlayer1WinState from an xml file
+     * @return true -> success, false -> failed to parse a gameState file
+     */
     bool onEnter() override;
+
+    /**
+     * This method cleans up at the end of MultiPlayer1WinState
+     * @return true
+     */
     bool onExit() override;
+
     std::string getStateID() const override;
 
 private:
