53630f626c5c2eef7436e548101779d4c23ad8e5
Andrej Becka <beckaand@pop-os.localdomain>
2023-06-12 03:31:54 +0200

    SEM - Write GameObjectFactory documentation

--- SemestralWork/src/Singletons/GameObjectFactory.cpp
+++ SemestralWork/src/Singletons/GameObjectFactory.cpp
@@ -11,12 +11,6 @@ GameObjectFactory* GameObjectFactory::Instance()
     return mInstance;
 }
 
-/**
- * This method registers a new type that is suitable for creation -> has a creator method
- * @param[in] typeID
- * @param[in] pCreator
- * @return ture -> successfully registered, false -> already registered
- */
 bool GameObjectFactory::registerType(const std::string& pTypeID, BaseCreator *pCreator)
 {
     auto it = mCreators.find(pTypeID);
@@ -30,25 +24,20 @@ bool GameObjectFactory::registerType(const std::string& pTypeID, BaseCreator *pC
     return true;
 }
 
-/**
- * This method creates an object (if registered) specified by an id
- * @param[in] typeID
- * @return nullptr -> failed to create a desired object, GameObject* -> successful creation
- */
 GameObject* GameObjectFactory::create(const std::string& pTypeID)
 {
     auto it = mCreators.find(pTypeID);
     if(it == mCreators.end())
     {
-        std::cout << "could not find type: " << pTypeID << std::endl;
+        std::cout << "Could not find type: " << pTypeID << "." <<std::endl;
         return nullptr;
     }
     BaseCreator* pCreator = (*it).second;
     return pCreator->createGameObject();
 }
 
-GameObjectFactory::GameObjectFactory() {}
+GameObjectFactory::GameObjectFactory() = default;
 
-GameObjectFactory::~GameObjectFactory() {}
+GameObjectFactory::~GameObjectFactory() = default;
 
 GameObjectFactory* GameObjectFactory::mInstance = nullptr;
